75f92e4dfd0333d96b75319ca6467e130bd60c8d
Nate Valla <77633870+Njv3788@users.noreply.github.com>
2022-09-03 17:28:04 -0400

    Almost there

--- Lab1/Lab1/Lab1_LIB.cpp
+++ Lab1/Lab1/Lab1_LIB.cpp
@@ -1,18 +1,57 @@
 #include "Lab1_LIB.h"
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
 #include <vector>
 
 using namespace std;
 
-void Ballon_Alt(vector<double> alt_value, vector<double> polynomial) {
-
-	for (float i = 0; i < 49; i += 1) {
-		cout << i << endl;
-		alt_value.push_back(i);
+void Ballon_Alt(vector<double>& alt_value, vector<double> polynomial,double step,double end) {
+	
+	double alt;
+	
+	for (double i = 0; i <= (end); i += step) {
+		
+		alt  = polynomial.at(0) * pow(i, 4) + polynomial.at(1) * pow(i, 3) + polynomial.at(2) * pow(i, 2);
+	    alt += polynomial.at(3) * pow(i, 1)+ polynomial.at(4) * pow(i, 0);
+		alt_value.push_back(alt);
 	}
 }
 
-void Peak_Valley() {
+vector<double> Peak_Valley(vector<double> alt_value,double step,double end) {
+	
+	vector<double> min_max;
 
-	cout << "don't Worry This is a prototype" << endl;
+	for (int i = 1; i < int(end / step); i++) {
+		if (alt_value.at(i) > alt_value.at(i-1)) {
+			if (alt_value.at(i) > alt_value.at(i + 1)) {
+				cout << "PEAK at " << i*step << endl;
+				min_max.push_back(i);
+			}
+		}
+		else {
+			if (alt_value.at(i) < alt_value.at(i + 1)) {
+				cout << "VALLEY at " << i*step<< endl;
+				min_max.push_back(i);
+			}
+		}
+	}
+
+	return(min_max);
 }
+
+void outputs(vector<double> alt_value, double step, double end) {
+	ofstream deliverable;
+
+	string filename = "Lab1_deliverable.txt";
+	deliverable.open(filename.c_str());
+	deliverable << "Hello World!\n";
+	
+	for (int i = 1; i < int(end / step); i++) {
+		deliverable << i << "\n";
+	}
+	deliverable << endl;
+	deliverable.close();
+}
+
--- Lab1/Lab1/Lab1_LIB.h
+++ Lab1/Lab1/Lab1_LIB.h
@@ -6,9 +6,8 @@
 
 using namespace std;
 
-void Ballon_Alt(vector<double> alt_value, vector<double> polynomial);
-void Peak_Valley();
-
-
+void Ballon_Alt(vector<double>& alt_value, vector<double> polynomial,double step,double end);
+vector<double> Peak_Valley(vector<double>,double,double);
+void outputs(vector<double> alt_value, double step, double end);
 
 #endif
